df11ce077e39fd0d70e5357033e8a6c1576903f4
Shruthi reddy <shruthireddy@Shruthis-MacBook-Air.local>
2024-06-09 13:21:04 +0100

    my first commit

---
+++ oop_card_game.py
@@ -0,0 +1,59 @@
+# War Game: Object Oriented Programming #
+from random import shuffle
+
+class Card:
+    suits = ["spades", "hearts", "diamonds", "clubs"]
+
+    values = ["None", "None", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "king", "Ace"]
+
+    def __init__(self, v, s):
+        self.value = v
+        self.suit = s
+
+    def __lt__(self, c2):
+        if self.value < c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit < c2.suit:
+                return True 
+            else:
+                return False
+        return False
+    
+    def __gt__(self, c2):
+        if self.value > c2.value:
+            return True
+        if self.value == c2.value:
+            if self.suit > c2.suit:
+                return True 
+            else:
+                return False
+        return False
+    
+    def __repr__(self):
+        v = self.values[self.value] + " of " + self.suits[self.suit]
+        return v
+    
+card1 = Card(10,2)
+card2 = Card(11,4)
+print(card1 < card2)
+print(card1 > card2)
+print(card1)
+
+class Deck:
+    def __init__(self):
+        self.cards= []
+        for i in range(2, 15):
+            for j in range(4):
+                self.cards.append(Card(i, j))
+        shuffle(self.cards)
+
+    def rm_card(self):
+        if len(self.cards) == 0:
+            return 
+        return self.cards.pop()
+
+deck = Deck()
+
+for cards in deck.cards[:5]:
+    print(cards)
